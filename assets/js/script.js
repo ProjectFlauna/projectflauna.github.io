bde3c7316c81b61a6ff645691ed4f1706432bcb6
Theo <theo.eversdijk@gmail.com>
2021-11-03 12:58:48 +0100

    wednesday, pt. 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ console.log(`JavaScript loaded`);
 // Sidebar begin
 function toggleNav() {
     const sidenav = document.getElementById("mySidebar");
-    main = document.getElementById("main");
+    //main = document.getElementById("main");
     sidenav.style.width = sidenav.style.width === "250px" ? '0' : '250px';
-    main.style.marginLeft = main.style.marginLeft === "250px" ? '0' : '250px';
+    //main.style.marginLeft = main.style.marginLeft === "250px" ? '0' : '250px';
 }
 // Sidebar end
