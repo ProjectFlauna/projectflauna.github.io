40d89e3c7bb82717602abc23cffb8571a35eca68
Theo <theo.eversdijk@gmail.com>
2021-11-04 15:41:17 +0100

    thursday

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,5 @@ console.log(`JavaScript loaded`);
 // Sidebar begin
 function toggleNav() {
     const sidenav = document.getElementById("mySidebar");
-    //main = document.getElementById("main");
     sidenav.style.width = sidenav.style.width === "250px" ? '0' : '250px';
-    //main.style.marginLeft = main.style.marginLeft === "250px" ? '0' : '250px';
 }
-// Sidebar end
